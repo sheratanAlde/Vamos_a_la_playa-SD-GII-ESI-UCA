1e1d3885228232622f9d34d092434daae9f39a41
JordiGM <jordi.guetomatavera@alum.uca.es>
2019-05-03 20:24:53 +0200

    Incluidos los varemos de tiempo para ver la playa con las mejores condiciones

--- Vamos a la playa/elTiempo.py
+++ Vamos a la playa/elTiempo.py
@@ -1,16 +1,82 @@
 # -*- coding: utf-8 -*-
 from aemet import Aemet
 import json
-from datetime import datetime, date, time, timedelta
+from datetime import datetime
 
 def calculoOleaje(dato):
+    if(dato == "débil"):
+        return 9
+    if(dato == "moderado"):
+        return 6
+    if(dato == "fuerte"):
+        return 3
+    return 10
 
+def calculoViento(dato):
+    if(dato == "flojo"):
+        return 9
+    if(dato == "moderado"):
+        return 6
+    if(dato == "fuerte"):
+        return 3
+    return 10
+
+def calculoTemperaturas(sensacion, agua, maxima):
+    valorTemperatura = 0
+
+    if(sensacion == "suave"):
+        valorTemperatura = 9
+    if(sensacion == "calor agradable"):
+        valorTemperatura = 6
+    if(sensacion != "suave" and sensacion != "calor agradable"):
+        valorTemperatura = 3
+
+    if(int(agua) > 20):
+        valorTemperatura += 10
+    else:
+        if(int(agua) > 18):
+            valorTemperatura += 5
+        else:
+            valorTemperatura += 0
+
+    if(int(maxima) > 35):
+        valorTemperatura += 3
+    else:
+        if(int(maxima) > 30):
+            valorTemperatura += 6
+        else:
+            if(int(maxima) > 25):
+                valorTemperatura += 10
+            else:
+                if(int(maxima) > 20):
+                    valorTemperatura += 6
+                else:
+                    if(int(maxima) > 15):
+                        valorTemperatura += 3
+                    else:
+                        valorTemperatura += 0
+    return int(valorTemperatura/3)
+
+def calculoIndiceUV(dato):
+    if(int(dato) > 10):
+        return 0
+    else:
+        if(int(dato) > 7):
+            return 2
+        else:
+            if(int(dato) > 5):
+                return 4
+            else:
+                if(int(dato) > 2):
+                    return 8
+                else:
+                    return 10
 
 def situacionPlaya(codPlaya):
     ahora = datetime.now()
 
     aemet = Aemet()
-    playa = aemet.get_prediccion_especifica_playa(cod)
+    playa = aemet.get_prediccion_especifica_playa(codPlaya)
     data_string = json.dumps(playa)
     decode = json.loads(data_string)
     datosActual = decode['prediccion']['dia'][0]
