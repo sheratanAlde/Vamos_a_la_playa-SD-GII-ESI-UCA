c55740e95897357d1507b814266e1af85701a51e
JordiGM <jordi.guetomatavera@alum.uca.es>
2019-05-04 14:01:21 +0200

    El código ha sido reestructurado y comentado

--- Vamos a la playa/elTiempo.py
+++ Vamos a la playa/elTiempo.py
@@ -3,85 +3,88 @@ from aemet import Aemet
 import json
 from datetime import datetime
 
-def calculoOleaje(dato):
-    if(dato == "débil"):
+
+#Función que en base al dato de la AEMET del tipo de oleaje da una calificación
+def calculoOleaje (dato):
+    if dato == "débil":
         return 9
-    if(dato == "moderado"):
+    if dato == "moderado":
         return 6
-    if(dato == "fuerte"):
+    if dato == "fuerte":
         return 3
     return 10
 
+
+#Función que en base al dato de la AEMET de la fuerza del viento se da una calificación
 def calculoViento(dato):
-    if(dato == "flojo"):
+    if dato == "flojo":
         return 9
-    if(dato == "moderado"):
+    if dato == "moderado":
         return 6
-    if(dato == "fuerte"):
+    if dato == "fuerte":
         return 3
     return 10
 
+
+#Función que en base al dato de la AEMET de las distintas temperaturas se da una calificación
 def calculoTemperaturas(sensacion, agua, maxima):
     valorTemperatura = 0
 
-    if(sensacion == "suave"):
+    if sensacion == "suave":                                                                                            #Sensación termica
         valorTemperatura = 9
-    if(sensacion == "calor agradable"):
+    if sensacion == "calor agradable":
         valorTemperatura = 6
-    if(sensacion != "suave" and sensacion != "calor agradable"):
+    if sensacion != "suave" and sensacion != "calor agradable":
         valorTemperatura = 3
 
-    if(int(agua) > 20):
+    if int(agua) > 20:                                                                                                  #Temperatura del agua
         valorTemperatura += 10
+    elif int(agua) > 18:
+        valorTemperatura += 5
     else:
-        if(int(agua) > 18):
-            valorTemperatura += 5
-        else:
-            valorTemperatura += 0
+        valorTemperatura += 0
 
-    if(int(maxima) > 35):
+    if int(maxima) > 35:                                                                                                #Temperatura maxima
+        valorTemperatura += 3
+    elif int(maxima) > 30:
+        valorTemperatura += 6
+    elif int(maxima) > 25:
+        valorTemperatura += 10
+    elif int(maxima) > 20:
+        valorTemperatura += 6
+    elif int(maxima) > 15:
         valorTemperatura += 3
     else:
-        if(int(maxima) > 30):
-            valorTemperatura += 6
-        else:
-            if(int(maxima) > 25):
-                valorTemperatura += 10
-            else:
-                if(int(maxima) > 20):
-                    valorTemperatura += 6
-                else:
-                    if(int(maxima) > 15):
-                        valorTemperatura += 3
-                    else:
-                        valorTemperatura += 0
+        valorTemperatura += 0
+
     return int(valorTemperatura/3)
 
-def calculoIndiceUV(dato):
-    if(int(dato) > 10):
-        return 0
-    else:
-        if(int(dato) > 7):
-            return 2
-        else:
-            if(int(dato) > 5):
-                return 4
-            else:
-                if(int(dato) > 2):
-                    return 8
-                else:
-                    return 10
 
+#Función que en base al dato de la AEMET de la intensidad de UV se da una calificación
+def calculoIndiceUV(dato):
+    if int(dato) > 10:
+        return 0
+    elif int(dato) > 7:
+        return 2
+    elif int(dato) > 5:
+        return 4
+    elif int(dato) > 2:
+        return 8
+    else:
+        return 10
+
+
+#Por medio del código de una playa se devuelve una calificación de las condiciones de playa a nivel climatologico
 def situacionPlaya(codPlaya):
     ahora = datetime.now()
 
     aemet = Aemet()
-    playa = aemet.get_prediccion_especifica_playa(codPlaya)
+    playa = aemet.get_prediccion_especifica_playa(int(codPlaya))
     data_string = json.dumps(playa)
     decode = json.loads(data_string)
     datosActual = decode['prediccion']['dia'][0]
 
-    if(ahora.hour() > 14):
+    if ahora.hour > 13:                                                                                                 #Según la franja horaria en la que nos encontremos mirara el dato de por la mañana o por la tarde
         cielo = datosActual['estadoCielo']['descripcion2']
         viento = datosActual['viento']['descripcion2']
         oleaje = datosActual['oleaje']['descripcion2']
@@ -101,7 +104,6 @@ def situacionPlaya(codPlaya):
     puntuacion += calculoIndiceUV(indiceUv)
 
     return puntuacion/4
-
 
 
 def infoPlaya(cod):
