89c32f08c15330799971ccd43b24952877bc5cc5
groutwo <48999075+groutwo@users.noreply.github.com>
2019-05-04 19:25:12 +0200

    Update Tw_autentificar.py

--- Vamos a la playa/Tw_autentificar.py
+++ Vamos a la playa/Tw_autentificar.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 import tweepy
 
-################Autentificacion en twitter################
 def autentificar(consumer_key,consumer_secret,access_token,access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
