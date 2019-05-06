b5ea0819ec342977809002abbfa931eb2fd90932
groutwo <48999075+groutwo@users.noreply.github.com>
2019-05-06 22:21:37 +0200

    Update Tw_ComandoInvalido.py

--- Vamos a la playa/Tw_ComandoInvalido.py
+++ Vamos a la playa/Tw_ComandoInvalido.py
@@ -4,5 +4,5 @@ import Tw_lector
 def Tw_ComandoInvalido(tweetRecibido):
     mencion = Tw_lector.obtenerMencion(tweetRecibido)
     twitter = Tw_lector.autentificarTwitter()
-    cad = mencion + " comando no invalido, por favor prueba con \"estoy en (donde estés) y quiero hacer (lo que quieras hacer)\""
+    cad = mencion + " comando no valido, por favor prueba con \"estoy en (donde estés) y quiero hacer (lo que quieras hacer)\""
     twitter.update_status(cad)
